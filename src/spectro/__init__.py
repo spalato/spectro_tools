52375b69f6012647e2a007a4fe2a86275a4a9e26
spalato <7659022+spalato@users.noreply.github.com>
2025-08-08 16:39:05 +0200

    This doesn't work, and is not necessary. Rolling back. Version bump.

--- src/spectro/__init__.py
+++ src/spectro/__init__.py
@@ -1,8 +1,5 @@
 """
 Tools for spectroscopy.
 """
-try:
-    from ._version import __version__, __commit__
-except ImportError:
-    __version__ = "0.0.0"
-    __commit__ = "unknown"
+
+__version__ = "0.3"
